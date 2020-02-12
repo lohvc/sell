0bc028663d2f6216377a3e8315266ad08c6fc348
lohvc <453111462@qq.com>
2020-02-12 22:04:05 +0800

    更改数据库连接配置

--- src/main/java/com/imooc/sell/dataObject/OrderMaster.java
+++ src/main/java/com/imooc/sell/dataObject/OrderMaster.java
@@ -48,10 +48,10 @@ public class OrderMaster {
     /*支付状态，默认未支付*/
     private Integer payStatus = PayStatusEnum.WAIT.getCode();
 
-    /*创建时间*/
-    private Date createTime;
-
-    /*更新时间*/
-    private Date updateTime;
+//    /*创建时间*/
+//    private Date createTime;
+//
+//    /*更新时间*/
+//    private Date updateTime;
 
 }
--- src/main/java/com/imooc/sell/dataObject/ProductCategory.java
+++ src/main/java/com/imooc/sell/dataObject/ProductCategory.java
@@ -6,7 +6,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.sql.Timestamp;
+
 
 /**
  * \* User: lohvc
@@ -27,64 +27,18 @@ public class ProductCategory {
     /*类目编号*/
     private Integer categoryType;
 
-    private Timestamp createTime;
+//    private Timestamp createTime;
+//
+//    private Timestamp updateTime;
 
-    private Timestamp updateTime;
 
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
 
     public ProductCategory(String categoryName, Integer categoryType) {
         this.categoryName = categoryName;
         this.categoryType = categoryType;
     }
 
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
+    public ProductCategory(){
 
-    public Timestamp getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Timestamp updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public ProductCategory() {
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                ", categoryType=" + categoryType +
-                '}';
     }
 }
