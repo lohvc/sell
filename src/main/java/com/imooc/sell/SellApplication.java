78f5fdcc93534e575174203220c723656fc6bf71
lohvc <453111462@qq.com>
2020-02-13 15:59:09 +0800

    修改打包方式为war包并重写sellApplication

--- src/main/java/com/imooc/sell/SellApplication.java
+++ src/main/java/com/imooc/sell/SellApplication.java
@@ -2,12 +2,18 @@ package com.imooc.sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SellApplication {
+public class SellApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return super.configure(springApplicationBuilder);
+    }
 }
